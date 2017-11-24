a4b28f81c611a71029beb65e0ed807bfcabc7425
zhaoxiaofeng <zhaoxiaofeng@4096.me>
2017-11-24 15:53:19 +0800

    更新日志例子

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-Please see http://log4go.googlecode.com/
+# Please see http://log4go.googlecode.com/
 
-Installation:
+# Installation:
 - Run `goinstall log4go.googlecode.com/hg`
 
-Usage:
+# Usage:
 - Add the following import:
 import l4g "log4go.googlecode.com/hg"
 
-Acknowledgements:
+# Acknowledgements:
 - pomack
   For providing awesome patches to bring log4go up to the latest Go spec
+
+
+# example
+
+## console log write
--- examples/ConsoleLogWriter_Manual.go
+++ examples/ConsoleLogWriter_Manual.go
@@ -7,7 +7,22 @@ import (
 import l4g "github.com/bsed/log4go"
 
 func main() {
-	log := l4g.NewLogger()
+	//log := l4g.NewLogger()
+	log := make(l4g.Logger)
+	defer log.Close()
+
 	log.AddFilter("stdout", l4g.DEBUG, l4g.NewConsoleLogWriter())
 	log.Info("The time is now: %s", time.Now().Format("15:04:05 MST 2006/01/02"))
+
+	log.AddFilter("stdout", l4g.DEBUG, l4g.NewConsoleLogWriter())
+	log.Info("The time is now: %s", time.Now().Format("15:04:05 MST 2006/01/02"))
+
+	log.Info("The time is now: %s", time.Now().Format("15:04:05 MST 2006/01/02"))
+
+	time.Sleep(1 * time.Second)
+
+	log.Info("The time is now: %s", time.Now().Format("15:04:05 MST 2006/01/02"))
+
+	// This makes sure the filters is running
+	// time.Sleep(200 * time.Millisecond)
 }
