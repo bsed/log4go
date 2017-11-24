4be487855f62e660947cbd44801945e21599adf4
zhaoxiaofeng <zhaoxiaofeng@4096.me>
2017-11-24 15:26:07 +0800

    添加 log 包装

--- wrapper.go
+++ wrapper.go
@@ -4,8 +4,11 @@ package log4go
 
 import (
 	"errors"
-	"os"
 	"fmt"
+	. "github.com/kimiazhu/golib/stack"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -14,7 +17,24 @@ var (
 )
 
 func init() {
+	// auto load config from default position
 	Global = NewDefaultLogger(DEBUG)
+	file, _ := exec.LookPath(os.Args[0])
+	dir := filepath.Dir(file)
+	if _, err := os.Stat("log4go.xml"); !os.IsNotExist(err) {
+		Global.LoadConfiguration("log4go.xml")
+	} else if _, err := os.Stat(filepath.Join(dir, "/log4go.xml")); !os.IsNotExist(err) {
+		Global.LoadConfiguration(filepath.Join(dir, "log4go.xml"))
+	} else if _, err := os.Stat(filepath.Join(dir, "/conf/log4go.xml")); !os.IsNotExist(err) {
+		Global.LoadConfiguration(filepath.Join(dir, "/conf/log4go.xml"))
+	} else {
+		//fmt.Fprintf(os.Stderr, "log4go config not found, exec dir is: %s, u need to load it by yourself.\n", dir)
+	}
+}
+
+// setup by config string, not a config file
+func Setup(config []byte) {
+	Global.Config(config)
 }
 
 // Wrapper for (*Logger).LoadConfiguration
@@ -23,7 +43,7 @@ func LoadConfiguration(filename string) {
 }
 
 // Wrapper for (*Logger).AddFilter
-func AddFilter(name string, lvl level, writer LogWriter) {
+func AddFilter(name string, lvl Level, writer LogWriter) {
 	Global.AddFilter(name, lvl, writer)
 }
 
@@ -88,19 +108,19 @@ func Stdoutf(format string, args ...interface{}) {
 
 // Send a log message manually
 // Wrapper for (*Logger).Log
-func Log(lvl level, source, message string) {
+func Log(lvl Level, source, message string) {
 	Global.Log(lvl, source, message)
 }
 
 // Send a formatted log message easily
 // Wrapper for (*Logger).Logf
-func Logf(lvl level, format string, args ...interface{}) {
+func Logf(lvl Level, format string, args ...interface{}) {
 	Global.intLogf(lvl, format, args...)
 }
 
 // Send a closure log message
 // Wrapper for (*Logger).Logc
-func Logc(lvl level, closure func() string) {
+func Logc(lvl Level, closure func() string) {
 	Global.intLogc(lvl, closure)
 }
 
@@ -202,6 +222,25 @@ func Info(arg0 interface{}, args ...interface{}) {
 	}
 }
 
+// Utility for Access log messages (see Debug() for parameter explanation)
+// Wrapper for (*Logger).Info
+func Access(arg0 interface{}, args ...interface{}) {
+	const (
+		lvl = ACCESS
+	)
+	switch first := arg0.(type) {
+	case string:
+		// Use the string as a format string
+		Global.intLogf(lvl, first, args...)
+	case func() string:
+		// Log the closure (no other arguments used)
+		Global.intLogc(lvl, first)
+	default:
+		// Build a format string so that it will be similar to Sprint
+		Global.intLogf(lvl, fmt.Sprint(arg0)+strings.Repeat(" %v", len(args)), args...)
+	}
+}
+
 // Utility for warn log messages (returns an error for easy function returns) (see Debug() for parameter explanation)
 // These functions will execute a closure exactly once, to build the error message for the return
 // Wrapper for (*Logger).Warn
@@ -254,7 +293,7 @@ func Error(arg0 interface{}, args ...interface{}) error {
 
 // Utility for critical log messages (returns an error for easy function returns) (see Debug() for parameter explanation)
 // These functions will execute a closure exactly once, to build the error message for the return
-// Wrapper for (*Logger).Critical
+// Wrapper for (*Logger).Critical. This method will log the call stack
 func Critical(arg0 interface{}, args ...interface{}) error {
 	const (
 		lvl = CRITICAL
@@ -262,17 +301,86 @@ func Critical(arg0 interface{}, args ...interface{}) error {
 	switch first := arg0.(type) {
 	case string:
 		// Use the string as a format string
-		Global.intLogf(lvl, first, args...)
+		msg := fmt.Sprintf("%s\n%s", fmt.Sprintf(first, args...), CallStack(3))
+		Global.intLogf(lvl, msg)
+		//Global.intLogf(lvl, "%s", CallStack(3))
 		return errors.New(fmt.Sprintf(first, args...))
 	case func() string:
 		// Log the closure (no other arguments used)
 		str := first()
-		Global.intLogf(lvl, "%s", str)
+		Global.intLogf(lvl, "%s\n%s", str, CallStack(3))
+		//Global.intLogf(lvl, "%s", CallStack(3))
+		return errors.New(str)
+	case func(interface{}) string:
+		str := first(args[0])
+		Global.intLogf(lvl, "%s\n%s", str, CallStack(3))
 		return errors.New(str)
 	default:
 		// Build a format string so that it will be similar to Sprint
-		Global.intLogf(lvl, fmt.Sprint(first)+strings.Repeat(" %v", len(args)), args...)
+		msg := fmt.Sprintf("%s\n%s", fmt.Sprint(first) + fmt.Sprintf(strings.Repeat(" %v", len(args)), args...), CallStack(3))
+		Global.intLogf(lvl, msg)
 		return errors.New(fmt.Sprint(first) + fmt.Sprintf(strings.Repeat(" %v", len(args)), args...))
 	}
 	return nil
+}
+
+// Recover used to log the stack when panic occur.
+// usage: defer log4go.Recover("this is a msg: %v", "msg")
+// or:
+//      defer log4go.Recover(func(err interface{}) string {
+//          // ... your code here, return the error message
+//          return fmt.Sprintf("recover..v1=%v;v2=%v;err=%v", 1, 2, err)
+//      })
+func Recover(arg0 interface{}, args ...interface{}) {
+	if err := recover(); err != nil {
+		switch a := arg0.(type) {
+		case func(interface{}) string:
+			// the recovered err will pass to this func
+			Critical(arg0, append([]interface{}{err}, args)...)
+		case string:
+			Critical(a+"\n%v", append(args, err)...)
+		default:
+			Critical(arg0, append(args, err)...)
+		}
+	}
+}
+
+func IsFinestEnabled() bool {
+	return isLevelEnabled(FINEST)
+}
+
+func IsFineEnabled() bool {
+	return isLevelEnabled(FINE)
+}
+
+func IsDebugEnabled() bool {
+	return isLevelEnabled(DEBUG)
+}
+
+func IsTraceEnabled() bool {
+	return isLevelEnabled(TRACE)
+}
+
+func IsInfoEnabled() bool {
+	return isLevelEnabled(INFO)
+}
+
+func IsWarnEnabled() bool {
+	return isLevelEnabled(WARNING)
+}
+
+func IsErrorEnabled() bool {
+	return isLevelEnabled(ERROR)
+}
+
+func isLevelEnabled(lvl Level) bool {
+	enabled := false
+	for _, filt := range Global {
+		if lvl >= filt.Level {
+			// return true if any filt matched
+			enabled = true
+			break
+		}
+	}
+	return enabled
 }
